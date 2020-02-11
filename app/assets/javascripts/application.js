1b516f984700ffaec65bfecb3dac8b97c2b9ccb5
Philiplewis35 <philiplewis35@gmail.com>
2020-02-11 11:53:57 +0000

    different message passing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 $(document).ready(function() {
     $('.select_services').select2();
     if($('.home_page')[0]) {
-      send_services_to_chrome_extension(1)
+      send_services_to_chrome_extension_2(1)
     }
 });
 
@@ -14,5 +14,11 @@ function send_services_to_chrome_extension(user_id) {
   $.get("/users/" + user_id + "/services", {}, function(response) {
     window.postMessage({type: 'FROM_PAGE', user_id: 1, services: response}, "*")
     console.log('test')
+  })
+}
+
+function send_services_to_chrome_extension_2(user_id) {
+  $.get("/users/" + user_id + "/services", {}, function(response) {
+    chrome.runtime.sendMessage('dlmbcmlonnpdjankmfbpifocdhdeghec', {services: response}, console.log('done'));
   })
 }
