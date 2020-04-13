f5fdd6933a8d915559dee676feaff034081b3bf2
Philiplewis35 <philiplewis35@gmail.com>
2020-04-13 11:11:17 +0100

    make UI nicer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :about
 
   def show
     @user = current_user
     @services = Service.all
+  end
+
+  def about
   end
 end
