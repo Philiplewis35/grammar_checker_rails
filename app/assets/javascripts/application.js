538ded90a1c2863d57ba027f11fb54cd7a42419a
Philiplewis35 <philiplewis35@gmail.com>
2020-04-13 13:10:45 +0100

    fix select2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,23 +2,14 @@
 //= require jquery_ujs
 // require turbolinks
 //= require_tree .
+//
 
-$(document).ready(function() {
-    $('.select_services').select2();
-    if($('.home_page')[0]) {
-      send_services_to_chrome_extension_2(1)
+$(document).on("turbolinks:load", () => {
+  $('.select_services').select2({
+    "language": {
+        "noResults": function(){
+            return "Your are using all existing services";
+        }
     }
+  });
 });
-
-function send_services_to_chrome_extension(user_id) {
-  $.get("/users/" + user_id + "/services", {}, function(response) {
-    window.postMessage({type: 'FROM_PAGE', user_id: 1, services: response}, "*")
-    console.log('test')
-  })
-}
-
-function send_services_to_chrome_extension_2(user_id) {
-  $.get("/users/" + user_id + "/services", {}, function(response) {
-    chrome.runtime.sendMessage('dlmbcmlonnpdjankmfbpifocdhdeghec', {services: response}, console.log('done'));
-  })
-}
